1ce2ca0adda6140785b5d8a774486b2fff877271
MaximOEgorov <llkk67@gmail.com>
2024-01-25 00:23:45 +0300

    #Урок 61

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,10 +1,14 @@
 import logo from "../../logo.svg";
 import s from "./header.module.css"
+import {NavLink} from "react-router-dom";
 
 const Header = (props) => {
     return (
         <header className={s.App_header}>
             <img src={logo} className="App-logo" alt="logo"/>
+            <div className={s.loginBlock}>
+                <NavLink to={'/login'}>Login:</NavLink>
+            </div>
         </header>
     );
 }
