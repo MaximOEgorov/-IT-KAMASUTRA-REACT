53ef057b2a2aa3ddcee79477f414d93e61030a31
MaximOEgorov <llkk67@gmail.com>
2024-02-04 21:18:22 +0300

    #Урок 76 -- via formik

--- src/components/Profile/MyPosts/Post/MyPostForm.js
+++ src/components/Profile/MyPosts/Post/MyPostForm.js
@@ -1,10 +1,23 @@
 import React from "react";
-import {Formik, Form, Field} from "formik"
+import {Formik, Form, Field, ErrorMessage} from "formik"
+import styled from 'styled-components';
+
+const StyledErrorMessage = styled(ErrorMessage)`
+    color: red;
+    font-size: 12px;
+`;
 
 const MyPostForm = (props) => {
     return (
         <Formik
             initialValues={{newPostText: ""}}
+            validate={values=> {
+                const errors = {};
+                if (!values.newPostText) {
+                    errors.newPostText = 'Требуется заполнить поле';
+                }
+                return errors
+            }}
             onSubmit={(values, {resetForm}) => {
                 props.addPost(values.newPostText);
                 resetForm();
@@ -17,6 +30,7 @@ const MyPostForm = (props) => {
                                       placeholder="Enter your post hear"/>
                         )}
                     </Field>
+                    <StyledErrorMessage name="newPostText"/>
                 </div>
                 <div>
                     <button type={"submit"}>Add post</button>
